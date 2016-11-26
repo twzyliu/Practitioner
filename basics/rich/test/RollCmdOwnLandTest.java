1c2de70ee6c985baded6ab6c628071819313c260
Zhiyong Liu <zyongliu@thoughtworks.com>
2016-11-26 12:15:04 +0800

    finish task 13

--- basics/rich/test/RollCmdOwnLandTest.java
+++ basics/rich/test/RollCmdOwnLandTest.java
@@ -98,4 +98,17 @@ public class RollCmdOwnLandTest {
 
         assertThat(player.getAvailableCmdType() != availableCmdType, is(true));
     }
+
+    @Test
+    public void should_not_change_available_cmds_after_wrong_cmd() throws Exception {
+        Optional<Cmd> cmd = player.getAvailableCmd(TestHelper.ROLL_CMD);
+        player.execute(cmd);
+        Optional<Cmd> wrongCmd = player.getAvailableCmd(TestHelper.WRONG_CMD);
+        List<CmdType> availableCmdType = player.getAvailableCmdType();
+
+        player.execute(wrongCmd);
+
+        assertThat(player.getAvailableCmdType(), is(availableCmdType));
+
+    }
 }
