97fd4a19fba9fa2189bbd8e7982166aa069f52b0
Zhiyong Liu <zyongliu@thoughtworks.com>
2016-11-27 15:18:26 +0800

    finish task 44

---
+++ basics/rich/src/cmdType/RobotCmdType.java
@@ -0,0 +1,15 @@
+package cmdType;
+
+import cmd.Cmd;
+
+import java.util.Optional;
+
+/**
+ * Created by zyongliu on 27/11/16.
+ */
+public class RobotCmdType implements CmdType {
+    @Override
+    public Optional<Cmd> parse(String cmd) {
+        return null;
+    }
+}
