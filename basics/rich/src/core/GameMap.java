f1370ddcf794560934634c6d26681ceb9406106c
Zhiyong Liu <zyongliu@thoughtworks.com>
2016-11-28 23:27:30 +0800

    finish task 59

--- basics/rich/src/core/GameMap.java
+++ basics/rich/src/core/GameMap.java
@@ -2,11 +2,24 @@ package core;
 
 import place.Place;
 
+import java.util.List;
+
 /**
  * Created by zyongliu on 25/11/16.
  */
-public interface GameMap {
-    int move(int position);
-    Place getPlace(int position);
+public class GameMap {
+    private List<Place> places;
+
+    public GameMap(List<Place> places) {
+        this.places = places;
+    }
+
+    public int move(int position) {
+        return 0;
+    }
+
+    public Place getPlace(int position) {
+        return places.get(position % places.size());
+    }
 }
 
