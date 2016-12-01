7d0b28a43f120d2bb708fc1088d9d2eaf658e532
Zhiyong Liu <zyongliu@thoughtworks.com>
2016-12-02 01:40:34 +0800

    add render & edit function

--- ui/js1/editor.js
+++ ui/js1/editor.js
@@ -1,1 +1,34 @@
+$(document).ready(function () {
+    edit();
+    render();
+});
 
+
+var render = function () {
+    $('#render').each(function () {
+        $(this).click(
+            function () {
+                $('.contents').each(
+                    function () {
+                        var data = $(this).children().attr("data");
+                        $(this).children().replaceWith("<h1 class='render' data='" + data + "'>" + data + "</h1>");
+                    });
+            }
+        );
+    });
+};
+
+
+var edit = function () {
+    $('#edit').each(function () {
+        $(this).click(
+            function () {
+                $('.contents').each(
+                    function () {
+                        var data = $(this).children().attr("data");
+                        $(this).children().replaceWith("<input class='edit' data='" + data + "' value='" + data + "'/>");
+                    });
+            }
+        );
+    });
+};
