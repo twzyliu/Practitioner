649cb51baa5cfde0580af13a99f4acfe939d028d
Zhiyong Liu <zyongliu@thoughtworks.com>
2016-12-20 20:25:47 +0800

    add getAll

--- basics/orm1/src/main/orm/ActiveRecord.java
+++ basics/orm1/src/main/orm/ActiveRecord.java
@@ -1,30 +1,32 @@
 package main.orm;
 
 import java.lang.reflect.Field;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 
 /**
  * Created by zyongliu on 20/12/16.
  */
 public class ActiveRecord {
-    private HashMap<String, Object> cache = new HashMap<>();
+    private Statement statement;
+    private String id;
+    private Class<? extends ActiveRecord> aClass;
+    private String table;
+    private Field[] fields;
 
-    private void init() {
-        HashMap<String, Object> set = new HashMap<>();
-        Class<? extends ActiveRecord> aClass = this.getClass();
 
-        if (cache.containsKey(aClass.getName())) {
-            return;
-        }
+    private void init() throws SQLException {
+        Connector connector = new Connector();
+        aClass = this.getClass();
         AREntity annotation = aClass.getAnnotation(AREntity.class);
-        Field[] fields = aClass.getFields();
-        Arrays.stream(fields).filter((f) -> f.isAnnotationPresent(Id.class)).map((f) -> set.put("id", getColumnName(f)));
-        set.put("class", aClass);
-        set.put("table", annotation.tableName());
-        set.put("fields", fields);
-        cache.put(aClass.getName(), set);
+        fields = aClass.getFields();
+        table = annotation.tableName();
+        Arrays.stream(fields).filter((f) -> f.isAnnotationPresent(Id.class)).map((f) -> id = getColumnName(f));
+        statement = connector.getConnection().createStatement();
     }
 
     private String getColumnName(Field f) {
@@ -32,9 +34,56 @@ public class ActiveRecord {
         return column.isEmpty() ? f.getName() : column;
     }
 
-    private <T extends ActiveRecord> List<T> getAll() {
+    private <T extends ActiveRecord> List<T> getAll() throws SQLException, IllegalAccessException, InstantiationException {
         init();
-        return null;
+        List<T> out = new ArrayList<>();
+        try {
+            ResultSet resultSet = statement.executeQuery("select * from " + table);
+            while (resultSet.next()) {
+                Object object = aClass.newInstance();
+                for (Field field : fields) {
+                    if (field.isAnnotationPresent(Column.class)) {
+                        String column = field.getAnnotation(Column.class).column();
+                        field.setAccessible(true);
+                        String columnLabel = column.isEmpty() ? field.getName() : column;
+                        if (field.getType() == byte.class) {
+                            byte aByte = resultSet.getByte(columnLabel);
+                            field.set(object, aByte);
+                        } else if (field.getType() == short.class) {
+                            short aShort = resultSet.getShort(columnLabel);
+                            field.set(object, aShort);
+                        } else {
+                            field.set(object, resultSet.getObject(columnLabel, field.getType()));
+                        }
+                    }
+                }
+                out.add((T) object);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out;
     }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
